d50789cd5061384ecd1b4a23f7b89eecd1ee65f0
pribadisatya4-coding <pribadisatya4@gmail.com>
2026-09-12 10:35:50 +0800

    Update Supabase client URL and key format

--- sip-pilkades/src/supabaseClient.js
+++ sip-pilkades/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = https://hkohdhvlayglpfiuvqlg.supabase.co/rest/v1/
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhrb2hkaHZsYXlnbHBmaXV2cWxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNTg2OTksImV4cCI6MjEwNDczNDY5OX0.AjqtZ2cSXa74ErgnL2CYhLAS0hAVq-uqiYs6b-A03uw
+const supabaseUrl = 'https://hkohdhvlayglpfiuvqlg.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhrb2hkaHZsYXlnbHBmaXV2cWxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNTg2OTksImV4cCI6MjEwNDczNDY5OX0.AjqtZ2cSXa74ErgnL2CYhLAS0hAVq-uqiYs6b-A03uw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
